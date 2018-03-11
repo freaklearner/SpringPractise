bb506baf732f1706168544502ae9cf2839e43446
Shubham <shub.shubhamthakur@gmail.com>
2018-03-12 02:25:53 +0530

    contexConfixLocation with init-param practise

---
+++ ContextConfigExmp/src/org/helloController.java
@@ -0,0 +1,23 @@
+package org;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class helloController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String name = req.getParameter("str");
+		Map<String, String> m = new HashMap<>();
+		m.put("msg", "Hello "+ name);
+		
+		return new ModelAndView("success",m);
+	}
+	
+}
