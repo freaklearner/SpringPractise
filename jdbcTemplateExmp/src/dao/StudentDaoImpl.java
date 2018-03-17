e46ff0e13f5cae9c3fe1ff2d547f1a719b445dcc
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-18 05:07:48 +0530

    Studet Dao Implementation created

---
+++ jdbcTemplateExmp/src/dao/StudentDaoImpl.java
@@ -0,0 +1,29 @@
+package dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import model.Student;
+
+public class StudentDaoImpl implements StudentDao {
+
+	private JdbcTemplate js;
+	
+	public void setJs(JdbcTemplate js) {
+		this.js = js;
+	}
+	
+	@Override
+	public int save(Student st) {
+		try {
+			Object[] ar = {"abhi","abhi@gmail.com","Delhi"};
+			int i = js.update("insert into student(name,email,adrs) values(?,?,?)",ar);
+			return i;
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+
+}
