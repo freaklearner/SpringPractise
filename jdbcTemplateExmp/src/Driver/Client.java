947c47493cc14bbfe4a37227c176950a91f98294
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-18 05:18:54 +0530

    User's input option added

--- jdbcTemplateExmp/src/Driver/Client.java
+++ jdbcTemplateExmp/src/Driver/Client.java
@@ -1,5 +1,7 @@
 package Driver;
 
+import java.util.Scanner;
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,13 +14,23 @@ public class Client {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext cap = new ClassPathXmlApplicationContext("resources/web.xml");
 		StudentBo ab = (StudentBoImpl)cap.getBean("buss");
-		int i = ab.create(new Student("abhi", "abhi@gmail.com", "Delhi"));
+		Student st = new Student();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter name:");
+		st.setName(scan.nextLine());
+		System.out.println("enter Email:");
+		st.setEmail(scan.nextLine());
+		System.out.println("enter Address:");
+		st.setAddress(scan.nextLine());
+		int i = ab.create(st);
 		if(i>0) {
 			System.out.println("Record created Successfully");
 			
 		}else {
 			System.out.println("Record creation failed");
 		}
+		scan.close();
+		cap.close();
 		
 	}
 }
