5a2efe0431a05ff9a60b00b94c66f29efa3fb793
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-18 05:07:04 +0530

    model object class 'Student' created

---
+++ jdbcTemplateExmp/src/model/Student.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Student {
+	
+	public Student(String name,String email,String address) {
+		this.name = name;
+		this.email = email;
+		this.address = address;
+	}
+	private String name,email,address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String adress) {
+		this.address = adress;
+	}
+	
+	
+
+}
