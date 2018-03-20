4d8f32a41a46ccb74047cbfa8858b0743dfc4ccf
Shubham <shub.shubhamthakur@gmail.com>
2018-03-20 22:01:32 +0530

    @ModelAttributeExamp

--- ModelAttributeExmp/src/com/org/shub/controller/helloController.java
+++ ModelAttributeExmp/src/com/org/shub/controller/helloController.java
@@ -4,10 +4,12 @@ import java.util.Hashtable;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.org.shub.model.Student;
 
 
 @Controller
@@ -20,10 +22,12 @@ public class helloController {
 	}
 	
 	@RequestMapping(value="/formadmission.sh",method=RequestMethod.POST)
-	public ModelAndView result(@RequestParam Map<String, String> reqParam) {
+	public ModelAndView result(@ModelAttribute("student1") Student student1) {
 		
+		String name = student1.getUsername();
+		String loc = student1.getLocation();
 		Map<String, String> m = new Hashtable<>();
-		m.put("msg", "Name: "+reqParam.get("username")+ " Location:"+reqParam.get("location"));
+		m.put("msg", "Name: "+name+ " Location:"+loc);
 		return new ModelAndView("success",m);
 	}
 }
---
+++ ModelAttributeExmp/src/com/org/shub/model/Student.java
@@ -0,0 +1,23 @@
+package com.org.shub.model;
+
+public class Student {
+	
+	private String username;
+	private String location;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+
+}
