1ac3ce437737806268bc320691365f74d3057bee
Shubham <shub.shubhamthakur@gmail.com>
2018-03-31 00:56:26 +0530

    test BindigResult and spring taglig form

--- ModelAttributeExmp/src/com/org/shub/controller/helloController.java
+++ ModelAttributeExmp/src/com/org/shub/controller/helloController.java
@@ -5,12 +5,13 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.org.shub.model.Student;
+import com.org.shub.model.*;
 
 
 @Controller
@@ -30,9 +31,17 @@ public class helloController {
 	
 	@RequestMapping(value="/formadmission.sh",method=RequestMethod.POST)
 	public ModelAndView result(@ModelAttribute("student1") Student student1) {
-		
 		Map<String, Student> m = new Hashtable<>();
 		m.put("msg",student1);
 		return new ModelAndView("success",m);
 	}
+	
+	@RequestMapping(value="./test.sh",method=RequestMethod.POST)
+	public ModelAndView testResult(@ModelAttribute("student2") TestStudent student2,BindingResult result) throws Exception{
+		if(result.hasErrors()) {
+			return new ModelAndView("AdmissionForm");
+		}
+		return null;
+	}
+	
 }
---
+++ ModelAttributeExmp/src/com/org/shub/model/TestStudent.java
@@ -0,0 +1,21 @@
+package com.org.shub.model;
+
+public class TestStudent {
+
+	private String stname;
+	private long stcont;
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	public long getStcont() {
+		return stcont;
+	}
+	public void setStcont(long stcont) {
+		this.stcont = stcont;
+	}
+	
+	
+}
